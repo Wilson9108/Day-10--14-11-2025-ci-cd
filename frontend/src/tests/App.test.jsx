70f573107695add2fbf5dab58a74e97858573882
wilson9108 <wilson.pulivelugula@gmail.com>
2025-11-16 13:10:22 +0530

    installed vitest and setup the vitest in the frontend for testing

---
+++ frontend/src/tests/App.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import App from '../App'
+
+describe('App Component', () => {
+    it('renders heading text', () => {
+        render(<App />)
+    })
+
+    it('add two numbers correctly', () => {
+        const sum = 2 + 2
+        expect(sum).toBe(4)
+    })
+})
