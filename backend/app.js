e621604895541e8b4e3e823ad643a233ad651b52
wilson9108 <wilson.pulivelugula@gmail.com>
2025-11-16 22:17:56 +0530

    added getage route in the app.js

--- backend/app.js
+++ backend/app.js
@@ -10,4 +10,8 @@ app.get('/name',(req,res)=>{
     res.status(200).json({message:'wilson'})
 })
 
+app.get('/getage',(req,res)=>{
+    res.status(200).json({message:29})
+})
+
 module.exports={app}
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,4 @@ const {app} = require("./app")
 console.log("sung jinwoo")
 
 console.log("vaishnavi")
-
-
-
 app.listen(2025,()=>console.log("Your port is running on 2025"))
